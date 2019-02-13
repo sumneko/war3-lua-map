1ad6575eacaafd4759e6fe4cedd53c4024c9ecb1
最萌小汐 <sumneko@hotmail.com>
2019-02-13 15:59:05 +0800

    更新测试

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -1,5 +1,9 @@
 ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '测试英雄' then
-        local skill = unit:addSkill('测试技能', '技能', 1)
+        local skill = unit:addSkill('测试技能', '物品', 3)
+        ac.wait(1, function ()
+            unit:getPoint():createItem('测试物品')
+        end)
+        unit:add('力量', 100)
     end
 end)
