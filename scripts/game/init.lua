0860efad56d19cc708aad63da9598ba99abee6b6
最萌小汐 <sumneko@hotmail.com>
2019-02-14 14:19:53 +0800

    更新测试地图

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -1,9 +1,6 @@
 ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '测试英雄' then
         local skill = unit:addSkill('测试技能', '技能', 3)
-        ac.wait(1, function ()
-            unit:createItem('测试物品')
-        end)
     end
 end)
 
@@ -31,4 +28,16 @@ function mt:onRemove()
     print('失去 item')
 end
 
-ac.player(1):createShop('测试防御塔', ac.point(-500, -500), 0)
+ac.player(1):add('金币', 1000)
+ac.player(1):add('木材', 2000)
+ac.player(1):add('食物', 10)
+ac.player(1):add('食物上限', 20)
+
+ac.loop(0.1, function ()
+    ac.player(1):add('金币', 10)
+end)
+
+local shop = ac.player(1):createShop('测试防御塔', ac.point(-500, -500), 0)
+shop:setItem(1, '测试物品')
+shop:setItem(4, '测试物品')
+shop:setItem(11, '测试物品')
