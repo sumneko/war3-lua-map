55450b0275096cc212e567805683b5ab42df6497
最萌小汐 <sumneko@hotmail.com>
2019-02-15 11:10:39 +0800

    更新测试

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -16,6 +16,13 @@ end
 
 function mt:onCastShot()
     print('cast shot')
+    local tag = ac.textTag()
+        : text '测试'
+        : at(self:getOwner():getPoint())
+        : speed(0.2)
+        : show(function (player)
+            return player == ac.player(1)
+        end)
 end
 
 local mt = ac.item['测试物品']
@@ -34,10 +41,15 @@ ac.player(1):add('食物', 10)
 ac.player(1):add('食物上限', 20)
 
 ac.loop(0.1, function ()
-    ac.player(1):add('金币', 10)
+    ac.player(1):add('金币', 100)
 end)
 
-local shop = ac.player(1):createShop('测试防御塔', ac.point(-500, -500), 0)
+local shop = ac.player(1):createShop('商店', ac.point(-500, -500), 0)
 shop:setItem('测试物品', 1)
 shop:setItem('测试物品', 4)
 shop:setItem('测试物品', 11)
+
+local shop2 = ac.player(2):createShop('商店', ac.point(-500, -500), 0)
+shop2:setItem('测试物品', 3)
+shop2:setItem('测试物品', 7)
+shop2:setItem('测试物品', 10)
