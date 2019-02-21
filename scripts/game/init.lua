5e23d4e5aa2c678728c2926457ae08ebf96adf87
最萌小汐 <sumneko@hotmail.com>
2019-02-21 18:08:25 +0800

    更新测试地图

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -1,39 +1,12 @@
 ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '测试英雄' then
         local skill = unit:addSkill('测试技能', '技能', 3)
+        unit:bagSize(6)
+        unit:event('单位-属性变化', function (_, _, k, v)
+            print(k, v)
+        end)
     end
 end)
-
-local mt = ac.skill['测试物品']
-
-function mt:onAdd()
-    print('获得 skill')
-end
-
-function mt:onRemove()
-    print('失去 skill')
-end
-
-function mt:onCastShot()
-    print('cast shot')
-    local tag = ac.textTag()
-        : text '测试'
-        : at(self:getOwner():getPoint())
-        : speed(0.2)
-        : show(function (player)
-            return player == ac.player(1)
-        end)
-end
-
-local mt = ac.item['测试物品']
-
-function mt:onAdd()
-    print('获得 item')
-end
-
-function mt:onRemove()
-    print('失去 item')
-end
 
 ac.player(1):add('金币', 1000)
 ac.player(1):add('木材', 2000)
@@ -41,7 +14,7 @@ ac.player(1):add('食物', 10)
 ac.player(1):add('食物上限', 20)
 
 ac.loop(0.1, function ()
-    ac.player(1):add('金币', 10)
+    ac.player(1):add('金币', 100)
 end)
 
 local shop = ac.player(16):createShop('商店', ac.point(-500, -500), 0)
