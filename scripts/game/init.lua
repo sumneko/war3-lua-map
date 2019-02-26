274016b4d0f89f2fc905370ffc88673e35568687
最萌小汐 <sumneko@hotmail.com>
2019-02-26 13:26:21 +0800

    更新测试地图

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -2,9 +2,6 @@ ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '测试英雄' then
         local skill = unit:addSkill('测试技能', '技能', 3)
         unit:bagSize(6)
-        unit:event('单位-属性变化', function (_, _, k, v)
-            print(k, v)
-        end)
     end
 end)
 
@@ -13,11 +10,32 @@ ac.player(1):add('木材', 2000)
 ac.player(1):add('食物', 10)
 ac.player(1):add('食物上限', 20)
 
+local message = require 'jass.message'
 ac.loop(0.1, function ()
     ac.player(1):add('金币', 100)
 end)
 
 local shop = ac.player(16):createShop('商店', ac.point(-500, -500), 0)
-shop:setItem('测试物品', 1)
-shop:setItem('测试物品', 4)
-shop:setItem('测试物品', 11)
+shop:setItem('测试物品', 1, 'Q')
+shop:setItem('测试物品', 4, 'W')
+shop:setItem('测试物品', 11, 'E')
+
+local mt = ac.item['测试物品']
+
+function mt:onAdd()
+    print('add item')
+end
+
+function mt:onRemove()
+    print('remove item')
+end
+
+local mt = ac.skill['测试物品']
+
+function mt:onAdd()
+    print('add skill')
+end
+
+function mt:onRemove()
+    print('remove skill')
+end
--- scripts/skill/测试技能.lua
+++ scripts/skill/测试技能.lua
@@ -12,25 +12,33 @@ local count = 0
 
 function mt:onCastShot()
     local unit = self:getOwner()
+    local target = self:getTarget()
     print('onCastShot', unit:currentSkill())
 
-    local jass = require 'jass.common'
-    local japi = require 'jass.japi'
-    count = count + 1
-    if count == 1 then
-        unit:bagSize(6)
-    elseif count == 2 then
-        unit:bagSize(1)
-    elseif count == 3 then
-        unit:bagSize(0)
-    elseif count == 4 then
-        unit:bagSize(3)
-    end
+    local eff = ac.effect {
+        model = [[units\human\HeroArchMage\HeroArchMage.mdx]],
+        target = target,
+        size = 2,
+        yScale = 1,
+        angle = 90,
+        time = 1,
+        height = 100,
+        skipDeath = true,
+        sight = function (player)
+            return player == ac.player(1)
+        end
+    }
 
-    for _, u in ac.selector()
-        : inLine(unit, unit:getPoint() * self:getTarget(), unit:getPoint() / self:getTarget(), 500)
-        : ipairs()
-    do
-        print(u)
-    end
+    ac.timer(0.1, 50, function ()
+        print(self:getCd())
+    end)
+
+    ac.wait(1, function ()
+        self:setCd(0)
+    end)
+    ac.wait(2, function ()
+        unit:set('冷却缩减', 40)
+        self:activeCd(20)
+        self:setCd(1)
+    end)
 end
