1bfe5abb560956f3dcfe1b4d3173060c36ce5485
最萌小汐 <sumneko@hotmail.com>
2019-02-28 15:59:16 +0800

    更新演示图

--- scripts/skill/测试技能.lua
+++ scripts/skill/测试技能.lua
@@ -15,11 +15,16 @@ function mt:onCastShot()
     local target = self:getTarget()
     print('onCastShot', unit:currentSkill())
 
-    unit:addBuff '测试' {}
-    unit:addBuff '测试' {}
+    unit:addBuff '测试1' {}
+    unit:addBuff '测试2' {}
 end
 
-local mt = ac.buff['测试']
+local mt = ac.buff['测试1']
+
+mt.show = 1
+mt.icon = [[ReplaceableTextures\CommandButtons\BTNInvisibility.blp]]
+mt.title = '测试状态'
+mt.description = '测试状态的描述'
 
 function mt:onAdd()
     print('获得状态：', self)
@@ -29,6 +34,14 @@ function mt:onRemove()
     print('失去状态：', self)
 end
 
-function mt:onPulse()
-    print(self:remaining())
+local mt = ac.buff['测试2']
+
+mt.show = 1
+
+function mt:onAdd()
+    print('获得状态：', self)
+end
+
+function mt:onRemove()
+    print('失去状态：', self)
 end
