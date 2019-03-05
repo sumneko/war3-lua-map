ab514b168294c1f3c55e148b615680edd8e81d14
最萌小汐 <sumneko@hotmail.com>
2019-03-05 10:50:20 +0800

    更新测试地图

--- scripts/game/init.lua
+++ scripts/game/init.lua
@@ -1,20 +1,20 @@
 ac.game:event('单位-创建', function (_, unit)
     if unit:getName() == '测试英雄' then
         unit:bagSize(6)
-        --unit:blink(ac.point(65, 70))
-
-        print('===============')
-        local rect = ac.rect '矩形区域 000'
-
-        function rect:onEnter(unit)
-            print('进入', unit)
-        end
-
-        function rect:onLeave(unit)
-            print('离开', unit)
-        end
-
+        unit:set('攻击', 99999999999999)
+        unit:set('护甲', 99999999999999)
+        unit:set('力量', 99999999999999)
+        unit:set('敏捷', 99999999999999)
+        unit:set('智力', 99999999999999)
     end
+end)
+
+ac.game:event('玩家-聊天', function (_, _, str)
+    local japi = require 'jass.japi'
+    local jass = require 'jass.common'
+    local unit = ac.player(1):getHero()
+    print(tonumber(str))
+    jass.SetHeroStr(unit._handle, tonumber(str), true)
 end)
 
 ac.player(1):add('金币', 1000)
@@ -27,8 +27,8 @@ ac.loop(0.1, function ()
     ac.player(1):add('金币', 100)
 end)
 
-
-local shop = ac.player(16):createShop('商店', ac.point(-500, -500), 0)
+local unit = ac.player(16):createUnit('商店', ac.point(-500, -500), 0)
+local shop = unit:createShop()
 shop:setItem('测试物品', 1, 'Q')
 shop:setItem('测试物品', 4, 'W')
 shop:setItem('测试物品', 11, 'E')
@@ -44,6 +44,16 @@ end
 
 function mt:onRemove()
     print('remove item')
+end
+
+function mt:onCanBuy(...)
+    print(...)
+    return true
+end
+
+function mt:onCanAdd(...)
+    print(...)
+    return true
 end
 
 local mt = ac.skill['测试物品']
